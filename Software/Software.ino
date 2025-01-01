0a562be618c01feda8698581f6b6ebf49abc17c7
Per Carlén <pelle@rickebasta.se>
2025-01-01 19:55:40 +0100

    async webserver working

--- Software/Software.ino
+++ Software/Software.ino
@@ -3,13 +3,17 @@
 #include <ESP32CAN.h>
 #include <CAN_config.h>
 #include <WiFi.h>
-#include <NetworkClient.h>
 #include <WiFiAP.h>
+#include "ESPAsyncWebServer.h"
+#include "AsyncTCP.h"
+#include <ctime>
 
+#include "index_html.cpp"
+
+AsyncWebServer server(80);
 
 const char *ssid = "lily";
 const char *password = "0123456789";
-NetworkServer web_server(80);
 
 CAN_device_t CAN_cfg;               // CAN Config
 unsigned long previousMillis = 0;   // will store last time a CAN Message was send
@@ -31,8 +35,6 @@ void setup() {
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
-  web_server.begin();
-  Serial.println("Server started");
 
   pinMode(CAN_SE_PIN, OUTPUT);
   digitalWrite(CAN_SE_PIN, LOW);
@@ -42,6 +44,7 @@ void setup() {
   CAN_cfg.rx_queue = xQueueCreate(rx_queue_size, sizeof(CAN_frame_t));
   ESP32Can.CANInit();
   Serial.println("Starting Leaf timer");
+  init_webserver();
 }
 
 
@@ -61,52 +64,6 @@ void loop() {
 
   CAN_frame_t rx_frame;  
 
-  NetworkClient client = web_server.accept();
-  if (client) {                     // if you get a client,
-    Serial.println("New Client.");  // print a message out the serial port
-    String currentLine = "";        // make a String to hold incoming data from the client
-    while (client.connected()) {    // loop while the client's connected
-      if (client.available()) {     // if there's bytes to read from the client,
-        char c = client.read();     // read a byte, then
-        Serial.write(c);            // print it out the serial monitor
-        if (c == '\n') {            // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {  // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(LED_BUILTIN, HIGH);  // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(LED_BUILTIN, LOW);  // GET /L turns the LED off
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-  }
 
   unsigned long currentMillis = millis();
 
@@ -212,3 +169,29 @@ void loop() {
     }
   }
 }
+
+void init_webserver() {
+
+  server.on("/logout", HTTP_GET, [](AsyncWebServerRequest* request) { request->send(401); });
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Start server
+  server.begin();
+}
+
+String processor(const String& var)
+{
+  Serial.printf("Var:%s\n",var);
+  if(var == "X") {
+    String content = "";
+    content += "SoC: " + String(soc) + "&#37;<br>";
+    content += "SoH: " + String(soh) + "&#37;<br>";
+    return content;
+  } 
+  return String();
+}
+
---
+++ Software/index_html.cpp
@@ -0,0 +1,20 @@
+const char index_html[] = R"rawliteral(
+<!doctypehtml><title>Leaf Timer</title><meta content="width=device-width"name=viewport><style>html{font-family:Arial;display:inline-block;text-align:center}h2{font-size:3rem}body{max-width:800px;margin:0 auto}</style>%X%
+)rawliteral";
+
+/* The above code is minified (https://kangax.github.io/html-minifier/) to increase performance. Here is the full HTML function:
+<!DOCTYPE HTML><html>
+<head>
+  <title>Leaf Timer</title>
+  <meta name="viewport" content="width=device-width">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 3.0rem;}
+    body {max-width: 800px; margin:0px auto;}
+  </style>
+</head>
+<body>
+%X%
+</body>
+</html>
+*/
